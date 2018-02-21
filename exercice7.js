7a639ed2f760977dbf9565a4368fae377aabb3cc
Nathan Lamothe <e1436741@cmaisonneuve.qc.ca>
2018-02-21 10:35:26 -0500

    Ajout de feuilles de style

--- exercice7.js
+++ exercice7.js
@@ -79,7 +79,7 @@ app.get('/trier/:cle/:ordre', (req, res) => {
 	let cle = req.params.cle
 	let ordre = (req.params.ordre == 'asc' ? 1 : -1)
 	let cursor = db.collection('adresse').find().sort(cle,ordre).toArray(function(err, resultat){
-		//ordre = -ordre
+		ordre = (req.params.ordre == 'asc' ? 'desc' : 'asc')
 		//console.log(req.url.split("/")[3])
 		//console.log(req.params.ordre);
 		res.render('adresses.ejs', {adresses: resultat, cle, ordre })
